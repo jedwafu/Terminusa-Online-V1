4aa5ed935ce7e28466ea0e89b31a80de7349736a
Joedel Lopez Dalioan <joedel.dalioan@yahoo.com>
2025-02-17 16:59:01 +0800

    fix: Add gevent monkey patching to web_app.py

--- web_app.py
+++ web_app.py
@@ -1,3 +1,6 @@
+from gevent import monkey
+monkey.patch_all()
+
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
