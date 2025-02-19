1c8c7b134ce47da8c7b7b7c7868bb9db03ebae4e
Joedel Lopez Dalioan <joedel.dalioan@yahoo.com>
2025-02-19 21:53:57 +0800

    fix: update revision ID in migration 004 to match migration chain

--- migrations/versions/004_add_missing_tables.py
+++ migrations/versions/004_add_missing_tables.py
@@ -1,7 +1,7 @@
 """add missing tables
 
-Revision ID: 004
-Revises: 003
+Revision ID: 004_add_missing_tables
+Revises: 003_add_user_model
 Create Date: 2025-02-17 17:30:00.000000
 
 """
@@ -9,8 +9,8 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = '004'
-down_revision = '003'
+revision = '004_add_missing_tables'
+down_revision = '003_add_user_model'
 branch_labels = None
 depends_on = None
 
