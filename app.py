cba432755858c9e0c51555bcdaf5b1d378f952c6
Joedel Lopez Dalioan <joedel.dalioan@yahoo.com>
2025-02-25 10:08:42 +0800

    initialization fixes

--- app.py
+++ app.py
@@ -22,9 +22,10 @@ def create_app():
     # Initialize database
     from models import db
     db.init_app(app)
-    app.app_context().push()
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     migrate = Migrate(app, db)
+
 
 
 
