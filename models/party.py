82a43ab135e2d956c587e7effc83575495b8b586
Joedel Lopez Dalioan <joedel.dalioan@yahoo.com>
2025-02-25 16:01:55 +0800

    "Added __table_args__ to models/party.py to enable extending existing table."

--- models/party.py
+++ models/party.py
@@ -3,6 +3,8 @@ from models import db
 
 class Party(db.Model):
     __tablename__ = 'parties'
+    __table_args__ = {'extend_existing': True}
+
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
