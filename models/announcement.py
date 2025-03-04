7c75d4f34ab7e5eb67475495fac49a9902279ccb
Joedel Lopez Dalioan <joedel.dalioan@yahoo.com>
2025-03-05 00:18:16 +0800

    `Added import from .user and updated author relationship in Announcement model`

--- models/announcement.py
+++ models/announcement.py
@@ -4,6 +4,7 @@ from .base import BaseModel, TimestampMixin
 from database import db
 from sqlalchemy import Column, String, Text, Integer, Boolean, ForeignKey
 from sqlalchemy.orm import relationship, backref
+from .user import User
 
 
 class Announcement(BaseModel, TimestampMixin):
@@ -17,7 +18,7 @@ class Announcement(BaseModel, TimestampMixin):
     
     # Author relationship
     author_id = Column(Integer, ForeignKey('users.id'), nullable=True)
-    author = relationship('models.user.User', foreign_keys=[author_id], backref=backref('authored_announcements', lazy='dynamic'))
+    author = relationship(User, foreign_keys=[author_id], backref=backref('authored_announcements', lazy='dynamic'))
 
 
     def __repr__(self):
